6f73efd6784450eb0f764c822106c01f27435d24
Sebastian Kreisel <kreisel.sebastian@gmail.com>
2016-10-29 18:42:18 +0200

    working sudoku gen

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Asocgen
 I want to re-discover groups of finite order. For that I need to find
-associative structures. So here I am searching for semigroups in haskell.
+associative structures. So here I am searching for them in haskell.
--- src/Main.hs
+++ src/Main.hs
@@ -2,12 +2,19 @@ module Main where
 
 import Data.List
 import Data.Maybe
+--import Data.Tree
+
+import Control.Monad
+--import Control.Monad.Loops
+
 
 type BinOp = (Int -> Int -> Int)
 type MTab = [Int]
 
+data Action = Fill | Up
+
 set :: [Int]
-set = [0,1,2,3]
+set = [0..3]
 
 maxEle :: Int
 maxEle = maximum set
@@ -19,19 +26,15 @@ sizeM = length set
 -- Main
 main :: IO ()
 main = do
-  let t0 = genInitial
-  let t1 = upEntry t0 1 1
-  let t2 = upEntryMaybe t1 1 2
-  let t3 = upEntryMaybe t2 1 3
-  case t3 of
-    Nothing -> putStrLn "Nothing"
-    Just t -> printMTab t
-
+  let t0 = [genInitial]
+  let (m, res) = doStep Fill (t0, [])
+  --mapM_ printMTab res
+  putStrLn $ show (length res)
 
 printMTab :: MTab -> IO ()
 printMTab mtab = putStrLn $ showMTab mtab
 
-showMTab mtab = intercalate "\n" (rows mtab)
+showMTab mtab = (intercalate "\n" (rows mtab)) ++ "\n"
 rows mtab = map (\x -> show $ selectRow x mtab) set
 
 -- Basics
@@ -53,14 +56,33 @@ isAsocOn f = null xs
   where xs = [a | a <- set, b <- set, c <- set, f (f a b) c /= f a (f b c)]
 
 
--- Gen
+-- Generation
+isComplete :: MTab -> Bool
+isComplete mtab = not (-1 `elem` mtab)
+
 genInitial :: MTab
 genInitial = concat $ set : (map row (tail set))
   where row m = m : (replicate (sizeM - 1) (-1))
 
-upEntryMaybe :: Maybe MTab -> Int -> Int -> Maybe MTab
-upEntryMaybe mtab r c | isNothing mtab = Nothing
-                      | otherwise = upEntry (fromJust mtab) r c
+doStep :: Action -> ([MTab], [MTab]) -> ([MTab], [MTab])
+doStep _ ([], res) = ([], res)
+doStep Fill (t : ts, res) = case procMTab Fill t of
+  Nothing -> doStep Up ((t : ts), res)
+  Just r -> case isComplete r of
+    True -> doStep Up (ts, r : res)
+    False -> doStep Fill (r : t : ts, res)
+doStep Up (t : ts, res) = case procMTab Up t of
+  Nothing -> doStep Up (ts, res)
+  Just r -> doStep Fill (r : ts, res)
+
+procMTab :: Action -> MTab -> Maybe MTab
+procMTab a mtab =
+  join $ fmap (tupleUp mtab) (toRC $ fmap (+ (w a)) (findIndex (== -1) mtab))
+  where w Fill = 0
+        w Up = -1
+
+tupleUp mtab = uncurry (upEntry mtab)
+toRC i = fmap (\x -> (x `quot` length set, x `mod` length set)) i
 
 upEntry :: MTab -> Int -> Int -> Maybe MTab
 upEntry mtab r c
